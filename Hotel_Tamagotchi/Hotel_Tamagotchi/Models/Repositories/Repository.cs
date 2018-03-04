ffa039fd42ef3a82d088f4fbaa965b2e3d54f4b8
ArjunAutar01 <asautar@avans.nl>
2018-03-04 10:00:56 +0100

    implemented repository pattern

---
+++ Hotel_Tamagotchi/Hotel_Tamagotchi/Models/Repositories/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Tamagotchi.Models.Repositories
+{
+    public abstract class Repository<T> : IRepository<T>
+    {
+        protected Hotel_TamagotchiContext _context; //Todo: instanciëren (_context = new Context)
+
+        public Repository(Hotel_TamagotchiContext context)
+        {
+            this._context = context;
+        }
+        public abstract void Create(T model);
+        public abstract void Delete(T model);
+        public abstract T Get(int id);
+        public abstract List<T> GetAll();
+        public abstract void Update(T model);
+    }
+}
---
+++ Hotel_Tamagotchi/Hotel_Tamagotchi/Models/Repositories/TamagotchiRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Tamagotchi.Models.Repositories
+{
+    public class TamagotchiRepository : Repository<Tamagotchi>
+    {
+        public TamagotchiRepository(Hotel_TamagotchiContext context) : base(context)
+        {
+        }
+
+        public override void Create(Tamagotchi model)
+        {
+            this._context.Tamagotchis.Add(model);
+            this._context.SaveChanges();
+        }
+
+        public override void Delete(Tamagotchi model)
+        {
+            this._context.Tamagotchis.Remove(model);
+            this._context.SaveChanges();
+        }
+
+        public override Tamagotchi Get(int id)
+        {
+            return this._context.Tamagotchis.Find(id);
+        }
+
+        public override List<Tamagotchi> GetAll()
+        {
+            return this._context.Tamagotchis.ToList();
+        }
+
+        public override void Update(Tamagotchi model)
+        {
+            this._context.Entry(model).State = EntityState.Modified;
+            this._context.SaveChanges();
+        }
+    }
+}
