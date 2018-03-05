ac750912afdb722a941b4905cd22a763f3768400
ArjunAutar01 <asautar@avans.nl>
2018-03-05 15:34:08 +0100

    created RoomViewModel

--- Hotel_Tamagotchi/Hotel_Tamagotchi/Models/ViewModels/RoomViewModel.cs
+++ Hotel_Tamagotchi/Hotel_Tamagotchi/Models/ViewModels/RoomViewModel.cs
@@ -7,5 +7,56 @@ namespace Hotel_Tamagotchi.Models.ViewModels
 {
     public class RoomViewModel
     {
+        public int ID
+        {
+            get
+            {
+                return _room.ID;
+            }
+        }
+        public int Size
+        {
+            get
+            {
+                return _room.Size;
+            }
+            set
+            {
+                _room.Size = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return _room.Type;
+            }
+            set
+            {
+                _room.Type = value;
+            }
+        }
+        public List<Tamagotchi> TamagotchiList
+        {
+            get
+            {
+                return _room.TamagotchiList;
+            }
+            set
+            {
+                _room.TamagotchiList = value;
+            }
+        }
+        public RoomViewModel()
+        {
+            this._room = new Room();
+        }
+
+        public RoomViewModel(Room room)
+        {
+            this._room = room;
+        }
+
+        private Room _room;
     }
 }
