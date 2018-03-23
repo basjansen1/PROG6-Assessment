85b720ec89a1fe76dd336b31c18051ea9fbd2e07
Autar <asautar@avans.nl>
2018-03-23 13:00:53 +0100

    Revert "Fucked"
    This reverts commit b80add86ebe9c60a0b93f148b24833f227b8ae70.

--- Hotel_Tamagotchi/Hotel_Tamagotchi/Controllers/ReservationController.cs
+++ Hotel_Tamagotchi/Hotel_Tamagotchi/Controllers/ReservationController.cs
@@ -120,12 +120,9 @@ namespace Hotel_Tamagotchi.Controllers
         public void Complete(RoomViewModel roomViewModel)
         {
             Room room = _roomRepository.Get(_detailsRoomVM.Room.ID);
-            List<Tamagotchi> SelectedTamagotchiList = _detailsRoomVM.Tamagotichis.Select(t => _tamagotchiRepository.Get(t.ID)).ToList(); // test
-            //room.TamagotchiList = SelectedTamagotchiList; 
-            //SelectedTamagotchiList.ForEach(t => _tamagotchiRepository.Update(t)); //hoeft in principe niet
-            SelectedTamagotchiList.ForEach(t => t.CurrentRoom = room);
-            SelectedTamagotchiList.ForEach(t => _tamagotchiRepository.Update(t));
-            //_roomRepository.Update(room);
+            room.TamagotchiList = _detailsRoomVM.Tamagotichis;
+            room.TamagotchiList.ForEach(t => _tamagotchiRepository.Update(t));
+            _roomRepository.Update(room);
         }
     }
 }
