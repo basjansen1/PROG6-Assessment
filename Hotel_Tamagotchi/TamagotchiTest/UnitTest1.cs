7e53a9679823c4c7cfd4d3ff7958ab2054304d3a
JansenBas <bas-realmadrid@hotmail.com>
2018-03-24 13:37:22 +0100

    initial commit

---
+++ Hotel_Tamagotchi/TamagotchiTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hotel_Tamagotchi;
+using Hotel_Tamagotchi.Models.ViewModels;
+using Hotel_Tamagotchi.Models;
+using Hotel_Tamagotchi.Models.Repositories;
+using Hotel_Tamagotchi.Controllers;
+
+namespace TamagotchiTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataSource(@"Provider=Microsoft.SqlServerCe.Client.4.0; Data Source=C:\Data\MathsData.sdf;", "Numbers")]
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IRoomRepository roomRepository = new RoomRepository();
+            ITamagotchiRepository tamagotchiRepository = new TamagotchiRepository();
+
+            ReservationController reservationController = new ReservationController();
+
+            // arrange  
+            RoomViewModel roomViewModel = new RoomViewModel();
+            Room room = new Room();
+            room.Size = 1;
+            roomViewModel.AmountOfTamagotchis = 1;
+            roomViewModel.Room = room;
+
+            // act  
+            
+
+            // assert  
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+    }
+}
---
+++ Hotel_Tamagotchi/TamagotchiTestProject/TamagotchiTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TamagotchiTestProject
+{
+    [TestClass]
+    public class TamagotchiTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
